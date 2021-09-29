2e8a30d0a5516a30fbb85caf424e7d73ba2343cf
tsubauaaa <tsubasa1173@gmail.com>
2021-09-29 17:58:52 +0900

    計り作った

---
+++ ar_mesurement_app/android/app/src/main/kotlin/com/tsubauaaa/ar_mesurement_app/ar_mesurement_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tsubauaaa.ar_mesurement_app.ar_mesurement_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ar_mesurement_app/lib/ar_measurement_screen.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
+import 'dart:math' as math;
+import 'package:collection/collection.dart';
+
+class ArMeasurementScreen extends StatefulWidget {
+  const ArMeasurementScreen({Key? key}) : super(key: key);
+
+  @override
+  _ArMeasurementScreenState createState() => _ArMeasurementScreenState();
+}
+
+class _ArMeasurementScreenState extends State<ArMeasurementScreen> {
+  late ARKitController _arKitController;
+  ARKitPlane? plane;
+  ARKitNode? node;
+  vector.Vector3? lastPosition;
+  String? anchorId;
+
+  @override
+  void dispose() {
+    _arKitController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Distance Tracker App'),
+        ),
+        body: Container(
+          child: ARKitSceneView(
+            showFeaturePoints: true,
+            planeDetection: ARPlaneDetection.horizontal,
+            onARKitViewCreated: onARViewCreated,
+            enableTapRecognizer: true,
+          ),
+        ),
+      );
+
+  void onARViewCreated(ARKitController arKitController) {
+    _arKitController = arKitController;
+    _arKitController.onAddNodeForAnchor = addAnchor;
+    _arKitController.onUpdateNodeForAnchor = updateAnchor;
+    _arKitController.onARTap = (List<ARKitTestResult> ar) {
+      final ARKitTestResult? planeTap = ar.firstWhereOrNull(
+        (tap) => tap.type == ARKitHitTestResultType.existingPlaneUsingExtent,
+      );
+      if (planeTap != null) {
+        onPlaneTapHandler(planeTap.worldTransform);
+      }
+    };
+  }
+
+  void addAnchor(ARKitAnchor anchor) {
+    if (anchor is! ARKitPlaneAnchor) {
+      return;
+    }
+    addPlane(_arKitController, anchor);
+  }
+
+  void addPlane(ARKitController controller, ARKitPlaneAnchor anchor) {
+    anchorId = anchor.identifier;
+    plane = ARKitPlane(
+      width: anchor.extent.x,
+      height: anchor.extent.z,
+      materials: [
+        ARKitMaterial(
+          transparency: 0.5,
+          diffuse: ARKitMaterialProperty.color(
+            Colors.white,
+          ),
+        ),
+      ],
+    );
+
+    node = ARKitNode(
+      geometry: plane,
+      position: vector.Vector3(anchor.center.x, 0, anchor.center.z),
+      rotation: vector.Vector4(1, 0, 0, -math.pi / 2),
+    );
+
+    controller.add(node!, parentNodeName: anchor.nodeName);
+  }
+
+  void updateAnchor(ARKitAnchor anchor) {
+    if (anchor.identifier != anchorId || anchor is! ARKitPlaneAnchor) {
+      return;
+    }
+
+    final ARKitPlaneAnchor planeAnchor = anchor;
+    node!.position =
+        vector.Vector3(planeAnchor.center.x, 0, planeAnchor.center.z);
+    plane!.width.value = planeAnchor.extent.x;
+    plane!.height.value = planeAnchor.extent.z;
+  }
+
+  void onPlaneTapHandler(Matrix4 transform) {
+    final position = vector.Vector3(
+      transform.getColumn(3).x,
+      transform.getColumn(3).y,
+      transform.getColumn(3).z,
+    );
+
+    final material = ARKitMaterial(
+      lightingModelName: ARKitLightingModel.constant,
+      diffuse: ARKitMaterialProperty.color(
+        const Color.fromRGBO(255, 153, 83, 1),
+      ),
+    );
+
+    final sphere = ARKitSphere(
+      radius: 0.003,
+      materials: [material],
+    );
+
+    final node = ARKitNode(
+      geometry: sphere,
+      position: position,
+    );
+
+    _arKitController.add(node);
+
+    if (lastPosition != null) {
+      final line = ARKitLine(
+        fromVector: lastPosition!,
+        toVector: position,
+      );
+      final lineNode = ARKitNode(geometry: line);
+
+      _arKitController.add(lineNode);
+
+      final distance = calculateDistanceBetweenPoints(position, lastPosition!);
+      final point = getMiddleVector(position, lastPosition!);
+      drawText(distance, point);
+    }
+    lastPosition = position;
+  }
+
+  String calculateDistanceBetweenPoints(vector.Vector3 A, vector.Vector3 B) {
+    final length = A.distanceTo(B);
+    return '${(length * 100).toStringAsFixed(2)} cm';
+  }
+
+  vector.Vector3 getMiddleVector(vector.Vector3 A, vector.Vector3 B) {
+    return vector.Vector3(
+      (A.x + B.x) / 2,
+      (A.y + B.y) / 2,
+      (A.z + B.z) / 2,
+    );
+  }
+
+  void drawText(String textDistance, vector.Vector3 point) {
+    final textGeometry = ARKitText(
+      text: textDistance,
+      extrusionDepth: 1,
+      materials: [
+        ARKitMaterial(
+          diffuse: ARKitMaterialProperty.color(
+            Colors.red,
+          ),
+        )
+      ],
+    );
+
+    const scale = 0.001;
+    final vectorScale = vector.Vector3(scale, scale, scale);
+    final node = ARKitNode(
+      geometry: textGeometry,
+      position: point,
+      scale: vectorScale,
+    );
+    _arKitController.getNodeBoundingBox(node).then(
+      (List<vector.Vector3> result) {
+        final minVector = result[0];
+        final maxVector = result[1];
+
+        final dx = (maxVector.x - minVector.x) / 2 * scale;
+        final dy = (maxVector.y - minVector.y) / 2 * scale;
+
+        final position = vector.Vector3(
+          node.position.x - dx,
+          node.position.y - dy,
+          node.position.z,
+        );
+        node.position = position;
+      },
+    );
+    _arKitController.add(node);
+  }
+}
